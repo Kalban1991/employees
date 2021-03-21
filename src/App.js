26e359da68c851ce2f95d6b64423fe5ae9903045
Nurseit Kalbaev <kalban1991@gmail.com>
2021-03-21 03:55:59 -0700

    adding changes on App.js and Single.js

--- src/App.js
+++ src/App.js
@@ -1,121 +1,155 @@
-import React, {Component} from 'react';
-import List from './components/list/List';
-import Search from './components/search/Search';
-import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
-import { Button } from 'reactstrap';
-import { v4 as uuidv4 } from 'uuid';
-import Single from './components/single/Single';
-import Add from './components/single/Add';
-import './app.scss';
+import React, { Component } from "react";
+import List from "./components/list/List";
+import Search from "./components/search/Search";
+import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
+import { Button } from "reactstrap";
+import { v4 as uuidv4 } from "uuid";
+import Single from "./components/single/Single";
+import Add from "./components/single/Add";
+import "./app.scss";
+
+const apiurl = "http://localhost:5000/";
 
 class App extends Component {
-    constructor(){
-        super();
-        this.state = {
-            employees : [],
-            isLoading: false,
-            addMode: false,
-            searchBy: '',
-            search:'',
-            selected: {}
-        }
-    }
+  constructor() {
+    super();
+    this.state = {
+      employees: [],
+      isLoading: false,
+      addMode: false,
+      searchBy: "",
+      search: "",
+      selected: {},
+    };
+  }
 
-    delete =(id) => {
-        const employees = this.state.employees.filter(emp=>{
-            if(emp.id === id){
-                return false
-            }
-            return true
-        })
-        this.setState({employees})
-    }
+  delete = (id) => {
+    const employees = this.state.employees.filter((emp) => {
+      if (emp.id === id) {
+        return false;
+      }
+      return true;
+    });
+    this.setState({ employees });
+  };
 
-    getApiData = () => {
-        this.setState({isLoading:true})
-        fetch('https://raw.githubusercontent.com/maratgaip/json/master/people.json')
-        .then(json=>json.json())
-        .then(employees=>this.setState({employees, isLoading:false}))
-    }
+  getApiData = () => {
+    this.setState({ isLoading: true });
+    fetch(`${apiurl}api/employees`)
+      .then((json) => json.json())
+      .then((employees) => this.setState({ employees, isLoading: false }));
+  };
 
-    componentDidMount(){
-        this.getApiData();
-    }
+  componentDidMount() {
+    this.getApiData();
+  }
 
-    getSearch = (e) => {
-        this.setState({search:e.target.value});
-    }
+  getSearch = (e) => {
+    this.setState({ search: e.target.value });
+  };
 
-    selectOnChange = (searchBy) => {
-        this.setState({searchBy});
-    }
-    setEmployee = (selected) => {
-        this.setState({selected});
-    }
-    updateEmployee = (employee) => {
-        const employees = this.state.employees.map(emp=>{
-            if(emp.id === employee.id) {
-                return employee
-            }
-            return emp
-        })
-        this.setState({employees});
-    }
+  selectOnChange = (searchBy) => {
+    this.setState({ searchBy });
+  };
+  setEmployee = (selected) => {
+    this.setState({ selected });
+  };
+  updateEmployee = (employee) => {
+    const employees = this.state.employees.map((emp) => {
+      if (emp.id === employee.id) {
+        return employee;
+      }
+      return emp;
+    });
+    this.setState({ employees });
+  };
 
-    addNew = () => this.setState({addMode:true})
-    
-    addModeClose = () => this.setState({addMode:false})
-    
-    addEmployee = (employee) => {
-        const { employees } = this.state;
-        employee.id = uuidv4();
-        employees.unshift(employee);
-        this.setState({employees})
+  addNew = () => this.setState({ addMode: true });
+
+  addModeClose = () => this.setState({ addMode: false });
+
+  addEmployee = (employee) => {
+    const { employees } = this.state;
+    employee.id = uuidv4();
+    employees.unshift(employee);
+    this.setState({ employees });
+  };
+
+  filter = () => {
+    const { employees, search, searchBy } = this.state;
+    const filteredEmployees = employees.filter((employee) => {
+      return searchBy.length
+        ? employee[searchBy].toLowerCase().includes(search.toLowerCase())
+        : true;
+    });
+    return filteredEmployees;
+  };
+
+  render() {
+    const { isLoading, search, searchBy, selected, employees } = this.state;
+
+    // Filtering / Searching by
+    const filteredEmployees = this.filter();
+
+    const loader = <div className="lds-dual-ring"></div>;
+    let content = isLoading ? (
+      loader
+    ) : (
+      <List
+        delete={this.delete}
+        setEmployee={this.setEmployee}
+        employees={filteredEmployees}
+      />
+    );
+    if (!isLoading && !filteredEmployees.length) {
+      content = <div className="not-found">Data Not Found</div>;
     }
-
-    filter = () => {
-        const {employees, search, searchBy} = this.state;
-        const filteredEmployees = employees.filter(employee => {
-            return searchBy.length ? employee[searchBy].toLowerCase().includes(search.toLowerCase()) : true;
-        })
-        return filteredEmployees
-    }
-
-    render(){
-        const { isLoading, search, searchBy, selected, employees } = this.state;
-        
-        // Filtering / Searching by  
-        const filteredEmployees = this.filter();
-
-        const loader = <div className="lds-dual-ring"></div>;
-        let content = isLoading ? loader : <List delete={this.delete} setEmployee={this.setEmployee} employees={filteredEmployees} />
-        if(!isLoading && !filteredEmployees.length){
-            content = <div className="not-found">Data Not Found</div>
-        }
-        return (
-            <Router>
-            <Switch>
-                <div className="container">
-                    <Route path="/" exact>
-                        <Search searchBy={searchBy} selectOnChange={this.selectOnChange} value={search} getSearch={this.getSearch} />
-                        <Button onClick={this.addNew} color="primary" className="float-right">Add Employee</Button>
-                        <Add addEmployee={this.addEmployee} onClose={this.addModeClose} addMode={this.state.addMode}/>
-                        {content}
-                    </Route>
-                    <Route path="/page/:page" exact>
-                        <Search searchBy={searchBy} selectOnChange={this.selectOnChange} value={search} getSearch={this.getSearch} />
-                        {content}
-                    </Route>
-                    <Route path="/employee/:id">
-                        <Single updateEmployee={this.updateEmployee} employees={employees} data={selected} />
-                    </Route>
-                </div>
-            </Switch>
-            </Router>
-        )
-
-    }
-    
+    return (
+      <Router>
+        <Switch>
+          <div className="container">
+            <Route path="/" exact>
+              <Search
+                searchBy={searchBy}
+                selectOnChange={this.selectOnChange}
+                value={search}
+                getSearch={this.getSearch}
+              />
+              <Button
+                onClick={this.addNew}
+                color="primary"
+                className="float-right"
+              >
+                Add Employee
+              </Button>
+              <Add
+                addEmployee={this.addEmployee}
+                onClose={this.addModeClose}
+                addMode={this.state.addMode}
+              />
+              {content}
+            </Route>
+            <Route path="/page/:page" exact>
+              <Search
+                searchBy={searchBy}
+                selectOnChange={this.selectOnChange}
+                value={search}
+                getSearch={this.getSearch}
+              />
+              {content}
+            </Route>
+            <Route path="/employee/:id">
+              <Single
+                updateEmployee={this.updateEmployee}
+                employees={employees}
+                data={selected}
+              />
+            </Route>
+          </div>
+        </Switch>
+      </Router>
+    );
+  }
 }
 
 export default App;
--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,91 +1,118 @@
-import React, { Component } from 'react';
-import classNames from 'classnames';
-import './list.scss'
-import { Link }  from 'react-router-dom';
-import { withRouter }  from 'react-router';
+import React, { Component } from "react";
+import classNames from "classnames";
+import "./list.scss";
+import { Link } from "react-router-dom";
+import { withRouter } from "react-router";
 
 class List extends Component {
-    constructor(props){
-        super();
-        const page = Number(props.match.params.page);
-        const currentPage = !isNaN(page) ? page : 1
-        this.state = {
-            limit: 20,
-            currentPage
-        }
+  constructor(props) {
+    super();
+    const page = Number(props.match.params.page);
+    const currentPage = !isNaN(page) ? page : 1;
+    this.state = {
+      limit: 20,
+      currentPage,
+    };
+  }
+
+  setPage = (currentPage) => {
+    this.setState({ currentPage });
+  };
+  delete = (id, e) => {
+    e.preventDefault(); // Prevents event to redirect using links(a)
+    this.props.delete(id);
+  };
+
+  render() {
+    const { employees } = this.props;
+    const { currentPage, limit } = this.state;
+    let end = currentPage * limit;
+    const start = end - limit;
+
+    const pagedData = employees.slice(start, end);
+    let pages = [];
+    for (let i = 0; i < employees.length / limit; i++) {
+      pages.push(i);
     }
 
-    setPage = (currentPage) => {
-        this.setState({currentPage})
-    }
-    delete = (id,e) => {
-        e.preventDefault(); // Prevents event to redirect using links(a)
-        this.props.delete(id)
+    if (end > employees.length) {
+      end = employees.length;
     }
 
-    render(){
-
-        const {employees} = this.props;
-        const {currentPage, limit} = this.state;
-        let end = currentPage * limit; 
-        const start = end - limit;
-
-        const pagedData = employees.slice(start,end)
-        let pages = [];
-        for(let i=0;i<(employees.length / limit);i++){
-            pages.push(i)
-        }
-
-        if(end > employees.length){
-            end = employees.length;
-        }
-
-        return (
-                <div>
-                    <div className="pagination">
-                        <h3>
-                            Showing {pagedData.length} data -  {start}-{end} out of {this.props.employees.length}
-                        </h3>
-                        <div onClick={()=>this.setPage(currentPage-1)} className="page" >Prev</div>
-                        {
-                            pages.map((item,ind)=>{
-                                const cl = {page:true, active:ind+1 === currentPage}
-                                return <Link  onClick={()=>this.setPage(ind+1)} to={`/page/${ind+1}`} className={classNames(cl)} >{ind+1}</Link>
-                            })
-                        }
-                        <div onClick={()=>this.setPage(currentPage+1)} className="page" >Next</div>
-                        
-                    </div>
-                <div className="list">
-                    <div className="row header">
-                        <div className="cell sm">#</div>
-                        <div className="cell">Fullname</div>
-                        <div className="cell">Email</div>
-                        <div className="cell">City</div>
-                        <div className="cell">State</div>
-                        <div className="cell">Action</div>
-                    </div>
-                    <div className="content">
-                        {
-                            pagedData.map((employee,ind)=>{
-                                const { id, first_name, last_name, email, city, state } = employee;
-                                return (
-                                <Link key={id} className="row" to={`/employee/${id}`}>
-                                    <div className="cell sm">{id}</div>
-                                    <div className="cell">{first_name} {last_name}</div>
-                                    <div onClick={(e)=>e.preventDefault()} title={email} className="cell">{email}</div>
-                                    <div className="cell">{city}</div>
-                                    <div className="cell">{state}</div>
-                                    <div className="cell delete" onClick={(e)=>this.delete(id,e)}>Delete</div>
-                                </Link>
-                                )
-                            })
-                        }
-                    </div>
-                </div>
-                </div>
-        )
-    }
+    return (
+      <div>
+        <div className="pagination">
+          <h3>
+            Showing {pagedData.length} data - {start}-{end} out of{" "}
+            {this.props.employees.length}
+          </h3>
+          <div onClick={() => this.setPage(currentPage - 1)} className="page">
+            Prev
+          </div>
+          {pages.map((item, ind) => {
+            const cl = { page: true, active: ind + 1 === currentPage };
+            return (
+              <Link
+                onClick={() => this.setPage(ind + 1)}
+                to={`/page/${ind + 1}`}
+                className={classNames(cl)}
+              >
+                {ind + 1}
+              </Link>
+            );
+          })}
+          <div onClick={() => this.setPage(currentPage + 1)} className="page">
+            Next
+          </div>
+        </div>
+        <div className="list">
+          <div className="row header">
+            <div className="cell sm">#</div>
+            <div className="cell">Fullname</div>
+            <div className="cell">Email</div>
+            <div className="cell">City</div>
+            <div className="cell">State</div>
+            <div className="cell">Action</div>
+          </div>
+          <div className="content">
+            {pagedData.map((employee, ind) => {
+              const {
+                _id,
+                first_name,
+                last_name,
+                email,
+                city,
+                state,
+              } = employee;
+              return (
+                <Link key={_id} className="row" to={`/employee/${_id}`}>
+                  <div className="cell sm">{_id}</div>
+                  <div className="cell">
+                    {first_name} {last_name}
+                  </div>
+                  <div
+                    onClick={(e) => e.preventDefault()}
+                    title={email}
+                    className="cell"
+                  >
+                    {email}
+                  </div>
+                  <div className="cell">{city}</div>
+                  <div className="cell">{state}</div>
+                  <div
+                    className="cell delete"
+                    onClick={(e) => this.delete(_id, e)}
+                  >
+                    Delete
+                  </div>
+                </Link>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default withRouter(List)
+export default withRouter(List);
